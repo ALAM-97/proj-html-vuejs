c57c32c76fed3c7f9359b4701d8409949f6c9c5c
ALAM-97 <alexamara97@gmail.com>
2021-10-11 18:03:21 +0200

    Section Four Added

---
+++ src/components/CourseCard.vue
@@ -0,0 +1,124 @@
+<template>
+    <ul>
+        <li v-for="(course, index) in courses" :key="index">
+            <img :src="course.img" alt="">
+            <div class="free" v-if="(course.free == true) ? 'free' : null">FREE</div>
+            <div class="infos">
+                <div class="price">${{course.price}}.<span>00</span></div>
+                <div class="course-title">{{course.title}}</div>
+                <div class="less-and-stud">
+                    <div class="lessons">
+                        <i class="far fa-file-alt"></i>
+                        {{course.lessons}} Lessons
+                    </div>
+                    <div class="students">
+                        <i class="far fa-user"></i>
+                        {{course.students}} Students
+                    </div>
+                </div>
+            </div>
+        </li>
+    </ul>
+</template>
+
+<script>
+export default {
+    name: 'CourseCard',
+    data() {
+        return {
+            courses: [
+                {
+                    img : require("../assets/images/course-02-480x298.jpg"),
+                    price : "40",
+                    title : "Learning to Write as a Professional Author",
+                    lessons : "20",
+                    students : "50",
+                    free: false
+                },
+                {
+                    img : require("../assets/images/stock-full-hd-03-480x298.jpg"),
+                    price : "0",
+                    title : "Customer-Centric Info-Tech Strategies",
+                    lessons : "24",
+                    students : "769",
+                    free: true
+                },
+                {
+                    img : require("../assets/images/stock-full-hd-04-480x298.jpg"),
+                    price : "19",
+                    title : "Open Programming Courses for everyone: python",
+                    lessons : "17",
+                    students : "62",
+                    free: false
+                }
+            ]
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../assets/style/common.scss";
+
+    ul {
+        display: flex;
+        justify-content: space-between;
+        li {
+            width: calc(100% / 3 - 1.25rem);
+            display: flex;
+            flex-direction: column;
+            height: 25rem;
+            border-radius: .3125rem;
+            position: relative;
+        }
+        
+        img {
+            border-radius: .3125rem .3125rem 0 0;
+            width: 100%;
+            height: 50%;
+        }
+        .free {
+                width: 70px;
+                height: 30px;
+                background-color: #ee7455;
+                color: $white;
+                text-align: center;
+                line-height: 30px;
+                position: absolute;
+                top: 0;
+                right: 0;
+            }
+        .infos {
+            width: 100%;
+            height: 50%;
+            padding: 1.5625rem;
+            background-color: $white;
+            border-radius: 0 0 .3125rem .3125rem;
+            .price {
+                font-size: 1.3rem;
+                font-weight: 600;
+                color: $detailsColor;
+                margin-bottom: 10px;
+                span {
+                    font-size: 1.1rem;
+                }
+            }
+            .course-title {
+                font-weight: 600;
+                font-size: 1.2rem;
+                margin-bottom: 30px;
+            }
+            .less-and-stud {
+                display: flex;
+                .lessons,
+                .students {
+                    font-size: .9rem;
+                    font-weight: 300;
+                    color: #9693aa;
+                    margin-right: 20px;
+                }
+            }
+        }
+    }
+
+</style>
---
+++ src/components/SectionFour.vue
@@ -0,0 +1,92 @@
+<template>
+    <div class="container">
+        <section>
+            <div class="section-title">
+                <div>JOIN MAXCOACH AT BEST</div>
+                <h4>Latest Online <span>Courses</span></h4>
+            </div>
+            <div class="services">
+                <CourseCard/>
+            </div>
+            <div class="courses-nav">
+                <i class="fas fa-circle active"></i>
+                <i class="fas fa-circle"></i>
+                <i class="fas fa-circle"></i>
+                <i class="fas fa-circle"></i>
+                <i class="fas fa-circle"></i>
+            </div>
+            <div class="courses-sub">
+                Control your personal preference settings to get notified about <br> appropiate courses.
+                <a href="#">View all courses <i class="fas fa-long-arrow-alt-right"></i></a>
+            </div>
+        </section>
+    </div>
+</template>
+
+<script>
+import CourseCard from "./CourseCard.vue"
+
+export default {
+    name: 'SectionFour',
+    components: {
+        CourseCard
+    },
+    
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../assets/style/common.scss";
+
+    .section-title {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        margin: 170px auto 50px auto;
+        div {
+                font-size: .9rem;
+                margin-left: .3125rem;
+            }
+            h4 {
+                font-size: 2.9rem;
+                margin: 1.25rem 0;
+                font-weight: 600;
+                line-height: 3.2rem;
+                span {
+                    color: $detailsColor;
+                    font-weight: 300;
+                }
+            }
+    }
+    .services {
+        display: flex;
+        justify-content: space-between;
+    }
+    .courses-nav {
+        width: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        margin: 40px auto;
+        i {
+            margin: 0 10px;
+            opacity: .3;
+            font-size: .6rem;
+            &.active {
+                opacity: .8;
+                font-size: .9rem
+            }
+        }
+    }
+    .courses-sub {
+        width: 100%;
+        text-align: center;
+        line-height: 2.1875rem;
+        a {
+            color: $darkGray;
+            font-weight: bold;
+        }
+    }  
+    
+
+</style>
